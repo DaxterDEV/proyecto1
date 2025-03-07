04727e929cc1a6e48d3065152448f4d5779a9db9
hdpelix3gp <zaybernez@gmail.com>
2025-03-07 13:41:58 -0400

    Lista Ordenada Agregada a index

---
+++ script.js
@@ -0,0 +1,15 @@
+const estudiantes = new Map();
+estudiantes.set("Ana", [85, 90, 78]);
+estudiantes.set("Carlos", [92, 88, 95]);
+estudiantes.set("Beatriz", [78, 85, 80]);
+estudiantes.set("Daniel", [95, 90, 93]);
+estudiantes.set("Elena", [60, 65, 70]);
+
+// Obtener todas las notas en un solo array
+const todasLasNotas = Array.from(estudiantes.values()).flat();
+
+// Calcular el promedio general
+const sumaTotal = todasLasNotas.reduce((acc, nota) => acc + nota, 0);
+const promedioGeneral = sumaTotal / todasLasNotas.length;
+
+console.log("El promedio de todas las notas es:", promedioGeneral.toFixed(2));
