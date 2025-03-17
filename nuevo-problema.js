3be5cf487a442b9dc8c7fc6901738cc9b5c2d02b
hdpelix3gp <zaybernez@gmail.com>
2025-03-17 01:29:51 -0400

    Entrega Ejercicios Javascript  #5

--- nuevo-problema.js
+++ nuevo-problema.js
@@ -1,19 +1,14 @@
-// filter(), includes("ERRO"), slit() y cojo el 2 índice que sería el texto de error
+// filter(), includes("ERROR"), slit() y cojo el 2 índice que sería el texto de error
 const logs = [
   "[2024-02-29] ERROR: Conexión fallida",
   "[2024-02-29] INFO: Usuario inició sesión",
   "[2024-02-29] ERROR: Fallo en la API",
   "[2024-02-29] WARNING: Uso alto de memoria"
 ];
-// Extraer solo los errores
-const errores = [];
-logs.filter((error) => {
-  let fix = error.includes("ERROR");
-  if (fix == true) {
-    const logsError = error.split(": ");
-    errores.push(logsError[1]);
-  }
-});
+const errores = logs
+  .filter((linea) => linea.includes("ERROR"))
+  .map((linea) => linea.split(": ")[1]);
+
 // a) Resultado esperado
 console.log(errores);
 // → ["Conexión fallida", "Fallo en la API"]
@@ -32,3 +27,41 @@ function contarPorTipo(logs) {
 //contarPorTipo(logs);
 console.log(contarPorTipo(logs));
 // → {Error: 2, Info: 1, Warning: 1}
+
+//---------------------------------------------------------------------------------------------------------------------
+//Dado un array de ventas, calcular:
+//Total de ventas
+//Ventas por categoría
+//Productos con más de $50 en ventas
+const ventas = [
+  { producto: "Laptop", categoria: "Electrónica", precio: 1000 },
+  { producto: "Mouse", categoria: "Electrónica", precio: 25 },
+  { producto: "Libro", categoria: "Educación", precio: 15 },
+  { producto: "Mochila", categoria: "Educación", precio: 60 }
+];
+const totalVentas = ventas.reduce((acc, venta) => {
+  acc += venta.precio;
+
+  return acc;
+}, 0);
+
+//PARTE 2
+const ventasPorCategoria = ventas.reduce((acc, vCategorias) => {
+  acc[vCategorias.categoria] =
+    (acc[vCategorias.categoria] || 0) + vCategorias.precio;
+
+  return acc;
+}, {});
+
+//Productos con más de $50 en ventas
+//CODIGO AQUI!
+const productosCaros = ventas
+  .filter((venta) => venta.precio > 50)
+  .map((venta) => venta.producto);
+
+// Total de ventas
+console.log(`Total de ventas: $${totalVentas}`);
+// Ventas por categoría
+console.log(ventasPorCategoria); // → { Electrónica: 1025, Educación: 75 }
+// Productos con más de $50 en ventas
+console.log(productosCaros); // → ["Laptop", "Mochila"]
